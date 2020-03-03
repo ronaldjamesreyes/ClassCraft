eda3951eeafe9a734458ba486de2b89e7ebf427c
Dallin Robbins <zeidokku@gmail.com>
2020-03-03 13:17:25 -0800

    Import Tesseract
    Tesseract files in Tesseract OCR folder

---
+++ ClassCraft/Assets/Tesseract OCR/Scripts/TesseractDriver.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using UnityEngine;
+
+public class TesseractDriver
+{
+    private TesseractWrapper _tesseract;
+
+    public string CheckTessVersion()
+    {
+        _tesseract = new TesseractWrapper();
+
+        try
+        {
+            string version = "Tesseract version: " + _tesseract.Version();
+            Debug.Log(version);
+            return version;
+        }
+        catch (Exception e)
+        {
+            string errorMessage = e.GetType() + " - " + e.Message;
+            Debug.LogError(errorMessage);
+            return errorMessage;
+        }
+    }
+
+    public void Setup()
+    {
+        _tesseract = new TesseractWrapper();
+
+        string datapath = Application.streamingAssetsPath + "/tessdata/";
+
+        if (_tesseract.Init("eng", datapath))
+        {
+            Debug.Log("Init Successful");
+        }
+    }
+
+    public string GetErrorMessage()
+    {
+        return _tesseract?.GetErrorMessage();
+    }
+
+    public string Recognize(Texture2D imageToRecognize)
+    {
+        return _tesseract.Recognize(imageToRecognize);
+    }
+
+    public Texture2D GetHighlightedTexture()
+    {
+        return _tesseract.GetHighlightedTexture();
+    }
+}
