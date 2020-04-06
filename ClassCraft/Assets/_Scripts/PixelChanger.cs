625e0a31d3faeb3ae0489e72211dc7a195aa8ef8
Dallin Robbins <zeidokku@gmail.com>
2020-04-06 16:34:22 -0700

    Implement Writing

---
+++ ClassCraft/Assets/_Scripts/PixelChanger.cs
@@ -0,0 +1,51 @@
+﻿// Write black pixels onto the GameObject that is located
+// by the script. The script is attached to the camera.
+// Determine where the collider hits and modify the texture at that point.
+//
+// Note that the MeshCollider on the GameObject must have Convex turned off. This allows
+// concave GameObjects to be included in collision in this example.
+//
+// Also to allow the texture to be updated by mouse button clicks it must have the Read/Write
+// Enabled option set to true in its Advanced import settings.
+
+using UnityEngine;
+using System.Collections;
+
+public class PixelChanger : MonoBehaviour
+{
+    public Camera cam;
+    public int widthMod = 0;
+
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+    }
+
+    void Update()
+    {
+        if (!Input.GetMouseButton(0))
+            return;
+
+        RaycastHit hit;
+        if (!Physics.Raycast(cam.ScreenPointToRay(Input.mousePosition), out hit))
+            return;
+
+        Renderer rend = hit.transform.GetComponent<Renderer>();
+        MeshCollider meshCollider = hit.collider as MeshCollider;
+
+        if (rend == null || rend.sharedMaterial == null || rend.sharedMaterial.mainTexture == null || meshCollider == null)
+            return;
+
+        Texture2D tex = rend.material.mainTexture as Texture2D;
+        Vector2 pixelUV = hit.textureCoord;
+        pixelUV.x *= tex.width;
+        pixelUV.y *= tex.height;
+
+        for (int x = -widthMod; x <= widthMod; x++ ) {
+            for (int y = -widthMod; y <= widthMod; y++) {
+                tex.SetPixel((int)pixelUV.x + x, (int)pixelUV.y + y, Color.white);
+            }
+        }
+        tex.Apply();
+    }
+}
