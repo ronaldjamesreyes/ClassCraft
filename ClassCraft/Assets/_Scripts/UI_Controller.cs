bd7dbda137f19ef01a32af902e7eb898c670b651
abilkaiyrov <49922607+abilkaiyrov@users.noreply.github.com>
2020-04-06 20:02:22 -0700

    Added Main Menu

---
+++ ClassCraft/Assets/_Scripts/UI_Controller.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UI_Controller : MonoBehaviour
+{
+	public RectTransform Start;
+    public RectTransform Lesson;
+    public RectTransform Math;
+    public RectTransform Exit;
+	
+	    public void butStartClicked()
+    {
+        Debug.Log("Button start clicked");
+        Math.transform.gameObject.SetActive(true);
+        Lesson.transform.gameObject.SetActive(true);
+		Start.transform.gameObject.SetActive(false);
+        Exit.transform.gameObject.SetActive(false);
+    }
+
+    public void butMathClicked()
+    {
+        Debug.Log("Button Math clicked");
+		SceneManagement.LoadScene(MathWorld);
+    }
+
+    public void butLesson()
+    {
+        Debug.Log("Button select clicked");
+		SceneManagement.LoadScene(LessonWorld);
+    }
+
+    public void butExitClicked()
+    {
+        Debug.Log("Button exit clicked");
+        Application.Quit();
+    }
+}
