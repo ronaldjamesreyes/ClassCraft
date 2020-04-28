0ba0d1c78426254b81d4a71c08b7f56c568ac602
Dallin Robbins <zeidokku@gmail.com>
2020-04-28 16:35:18 -0700

    Integrated Logo, Tesseract Reset and Tesseract Logic Change
    Weird issue with tesseract, won't recognize on first submit, only after that.

--- ClassCraft/Assets/_Scripts/Tesseract.cs
+++ ClassCraft/Assets/_Scripts/Tesseract.cs
@@ -1,32 +1,32 @@
 ﻿using TMPro;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Tesseract : MonoBehaviour {
     [SerializeField] private Texture2D imageToRecognize;
     [SerializeField] private TextMeshPro displayText;
-    // [SerializeField] private RawImage outputImage;
     private TesseractDriver _tesseractDriver;
     private string _text = "";
     private Texture2D _texture;
 
     private void Start() {
-        // set up to read text from image
-
-        // start recognition calls
-        _tesseractDriver = new TesseractDriver();
-        Recognize();
     }
 
-    private void Recognize() {
+    public void Recognize() {
+        _tesseractDriver = new TesseractDriver();
         Texture2D texture = new Texture2D(imageToRecognize.width, imageToRecognize.height, TextureFormat.ARGB32, false);
         texture.SetPixels32(imageToRecognize.GetPixels32());
         texture.Apply();
 
         _texture = texture;
         _text = "";
-        // AddToTextDisplay(_tesseractDriver.CheckTessVersion());
+        _text = _text + "Tesseract is working";
         _tesseractDriver.Setup(OnSetupCompleteRecognize);
+    }
+
+    public void Reset() {
+        AssetDatabase.ImportAsset("Assets/Textures/Answer.png");
     }
 
     private void OnSetupCompleteRecognize() {
